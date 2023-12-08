92617377ede1fc4f868685b6c5b7507f7946af64
mhmtkarakas <m.karakas32@gmail.com>
2023-12-08 23:57:24 +0300

    initialize project

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import WatchList from './components/WatchList';
+import Watched from './components/Watched';
+import Add from './components/Add';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/"  element={<WatchList />}/>
+        <Route path="/watched"  element={<Watched />}/>
+        <Route path="/add"  element={<Add />}/>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
